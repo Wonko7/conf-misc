54ec37dba3656e3c6c887c62bc74ce620351c186
William <William@undefined.re>
2014-01-07 17:26:03 +0100

    add vimperator, xresources

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,19 +3,21 @@ ln -s ~/conf/vim/gvimrc ~/.gvimrc
 ln -s ~/conf/vim/vim ~/.vim
 
 ln -s ~/conf/zsh/zshrc ~/.zshrc
-
 ln -s ~/conf/git/gitconfig ~/.gitconfig
+ln -s ~/conf/misc/ackrc ~/.ackrc
+ln -s ~/conf/misc/mostrc ~/.mostrc
+ln -s ~/conf/misc/vimperatorrc ~/.vimperatorrc
 
 ln -s ~/conf/xmonad ~/.xmonad
 
 ln -s ~/conf/misc/xmodmap ~/.xmodmap
+ln -s ~/conf/misc/lua ~/.lua
 ln -s ~/conf/misc/xinitrc ~/.xinitrc
 ln -s ~/conf/misc/xinitrc ~/.xsession
 ln -s ~/conf/misc/Xdefaults ~/.Xdefaults
-ln -s ~/conf/misc/ackrc ~/.ackrc
 ln -s ~/conf/misc/conkyrc ~/.conkyrc
-ln -s ~/conf/misc/mostrc ~/.mostrc
 ln -s ~/conf/misc/lua ~/.lua
 
 
-echo "don't forget xmodmap -> check if default is good. also, you may want xbindkeys"
+
+echo "don't forget xmodmap -> check if default is good. also, you may want xbindkeys. also, can't remember if I actually used xresources."
