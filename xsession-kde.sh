f4a982248abd36af2bf6d9e2fdf96584c53a4e19
William <William@undefined.re>
2016-12-07 20:59:09 +0100

    stuffs

--- xsession-kde.sh
+++ xsession-kde.sh
@@ -1,8 +1,21 @@
 #! /bin/sh
 
 compton --config ~/.compton.conf&
-xbindkeys&
-(setxkbmap dvorak; xmodmap ~/conf/misc/xmodmap.laptop.dvorak&)&
-#(conky 2>&1 >> ~/conky.log &)&
-#conky >> ~/conky.log
-
+xset +fp /usr/share/fonts/terminus&
+xrdb -merge ~/.Xresources&
+#xbindkeys&
+setxkbmap dvorak&
+xmodmap ~/conf/misc/xmodmap.laptop.dvorak&
+#feh --bg-scale ~/docs/wallpapers/firefly.jpg
+#feh --bg-fill ~/docs/wallpapers/firefly.jpg&
+feh --bg-scale ~/docs/wallpapers/iss/scand.jpg&
+#klipper&
+#qlipper&
+(xscreensaver -no-splash && xscreensaver-command -lock&)
+#while true; do
+#	if pgrep xscreensaver; then
+#		xscreensaver-command -lock&
+#		break;
+#	fi;
+#	sleep 0.5;
+#done
