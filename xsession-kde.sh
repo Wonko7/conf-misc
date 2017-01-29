fc23509676090c3c4bdd9caa2397647c1b17b277
William <William@undefined.re>
2017-01-29 17:54:49 +0100

    misc x init stuff

--- xsession-kde.sh
+++ xsession-kde.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
 
 compton --config ~/.compton.conf&
-xset +fp /usr/share/fonts/terminus&
-xrdb -merge ~/.Xresources&
+
+(xset +fp /usr/share/fonts/terminus; xrdb -merge ~/.Xresources; xscreensaver -no-splash&)
 #xbindkeys&
-setxkbmap dvorak&
-xmodmap ~/conf/misc/xmodmap.laptop.dvorak&
+(setxkbmap dvorak; xmodmap ~/conf/misc/xmodmap.laptop.dvorak)&
 #feh --bg-scale ~/docs/wallpapers/firefly.jpg
 #feh --bg-fill ~/docs/wallpapers/firefly.jpg&
-feh --bg-scale ~/docs/wallpapers/iss/scand.jpg&
+feh --bg-fill ~/docs/wallpapers/iss/saturn1.jpg&
 #klipper&
 #qlipper&
-(xscreensaver -no-splash && xscreensaver-command -lock&)
+xscreensaver-command -lock&
+dunst -config ~/.dunstrc&
 #while true; do
 #	if pgrep xscreensaver; then
 #		xscreensaver-command -lock&
