d2e231822187e4f1cded1259fac380b59055dbfe
William <William@undefined.re>
2018-02-20 13:15:37 +0100

    tmux, xsess, dunst, st, vimium

--- xsession-kde.sh
+++ xsession-kde.sh
@@ -2,12 +2,13 @@
 
 compton --config ~/.compton.conf&
 
-(xset +fp /usr/share/fonts/terminus; xrdb -merge ~/.Xresources; xscreensaver -no-splash&)
+(xset +fp /usr/share/fonts/terminus; xrdb -merge ~/.Xresources; xscreensaver -no-splash && xscreensaver-command -lock)&
+
 #xbindkeys&
 (setxkbmap dvorak; xmodmap ~/conf/misc/xmodmap.laptop.dvorak)&
 #feh --bg-scale ~/docs/wallpapers/firefly.jpg
-#feh --bg-fill ~/docs/wallpapers/firefly.jpg&
-feh --bg-fill ~/docs/wallpapers/iss/saturn1.jpg&
+feh --bg-fill ~/docs/wallpapers/iss/cas-blue-dot.jpg&
+#feh --bg-fill ~/docs/wallpapers/iss/saturn1.jpg&
 #klipper&
 #qlipper&
 #xscreensaver-command -lock&
@@ -15,10 +16,10 @@ dunst -config ~/.dunstrc&
 xset b 0 0 0&
 xset r rate 400 30&
 unclutter -idle 1&
-while true; do
-	if pgrep xscreensaver; then
-		xscreensaver-command -lock&
-		break;
-	fi;
-	sleep 0.5;
-done
+## while true; do
+## 	if pgrep xscreensaver; then
+## 		xscreensaver-command -lock&
+## 		break;
+## 	fi;
+## 	sleep 0.5;
+## done
