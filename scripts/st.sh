151c0cb9b5ca139e3bd2f227cf23a2cab627bd4b
William <William@undefined.re>
2019-07-21 12:54:33 +0200

    st.sh: merge daban-urnud & yggdrassil

--- scripts/st.sh
+++ scripts/st.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
+if [ $HOST = yggdrasill ]; then
+  small_size=15
+  big_size=35
+else
+  # daban-urnud
+  big_size=15
+  small_size=15
+fi
+
+
 # get current desktop nawe, which in my case is a [0-9]+:
 desktop=$(wmctrl -d | sed -nre 's/.*\*.*\s+([0-9]+)$/\1/p')
 
