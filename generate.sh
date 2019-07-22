e7ae3d090cc390065e8b3da6cdf145b48531c7ff
William <william@undefined.re>
2019-07-22 12:52:17 +0200

    Generate dunst config

--- generate.sh
+++ generate.sh
@@ -1,10 +1,24 @@
-#! /bin/sh
+#! /bin/dash
 
 cd $(dirname $0)
 pwd
+HOST=$(hostname)
+
+case $HOST in
+  yggdrasill)
+    border=40
+    conky_width=250
+    ;;
+  *)
+  #daban-urnud)
+    border=20
+    conky_width=150
+    ;;
+esac
 
 sed conky/conkyrc \
-  -re s/@WIDTH/250/g \
+  -re s/@WIDTH/$conky_width/g \
+  -re s/@GAP_X/$border/g \
   -re s/@TOP_WIDTH/9/g \
   -re s/@FONT_SIZE/22/g \
   -re s/@FONT_DATE_SIZE/15/g \
@@ -19,7 +33,8 @@ sed conky/conkyrc \
   > generated/yggdrasill.conkyrc
 
 sed conky/conkyrc \
-  -re s/@WIDTH/150/g \
+  -re s/@WIDTH/$conky_width/g \
+  -re s/@GAP_X/$border/g \
   -re s/@TOP_WIDTH/14/g \
   -re s/@FONT_SIZE/10/g \
   -re s/@FONT_DATE_SIZE/9/g \
@@ -33,10 +48,24 @@ sed conky/conkyrc \
   -re s/@WLAN/wlp4s0/g \
   > generated/daban-urnud.conkyrc
 
+## for yggdrasill:
+## xmonad border = 20 + 20 = 40.
+## conky's gap_x = 40.
+## dunst :
+half_border=$(( border / 2 ))
+quarter_border=$(( border / 4 ))
+total_conk=$(( border * 2 + conky_width ))
+dunst_width=$(( total_conk - border ))
+#echo ${dunst_width}x0+${half_border}+${half_border}
+
 sed dunstrc \
-  -re s/@WIDTH/150/g \
+  -re s/@GEOM/${dunst_width}x0+${half_border}+${half_border}/g \
+  -re s/@SEP/${half_border}/g \
+  -re s/@PAD/${quarter_border}/g \
   > generated/daban-urnud.dunstrc
 
 sed dunstrc \
-  -re s/@WIDTH/250/g \
+  -re s/@GEOM/${dunst_width}x0+${half_border}+${half_border}/g \
+  -re s/@SEP/${half_border}/g \
+  -re s/@PAD/${quarter_border}/g \
   > generated/yggdrasill.dunstrc
