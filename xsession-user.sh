bd0e196598ac62a36f76ebae627a649696b2d70b
William <william@undefined.re>
2019-07-22 09:40:01 +0200

    user script, correct conky conf path

--- xsession-user.sh
+++ xsession-user.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 
-export DISPLAY=":0"
-#export QT_QPA_PLATFORMTHEME="qt5ct" // env!
-#export QT_AUTO_SCREEN_SCALE_FACTOR="1.5"
+HOST=$(hostname)
 
 echo "export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" > /tmp/dbus-exports.sh
 chmod a+r /tmp/dbus-exports.sh
@@ -23,7 +21,7 @@ mute
 (compton --config ~/.compton.conf&)
 (xscreensaver -no-splash && xscreensaver-command -lock&)
 (python3.7 ~/conf/misc/systemd-lock-handler.py xscreensaver-command --lock&)
-(conky -c ~/conf/misc/conky/$HOST.conky &)
+(conky -c ~/conf/misc/conky/$HOST.conkyrc &)
 (sleep 1 && killall -s USR1 conky&)
 (dunst -config ~/.dunstrc 2>&1 > /home/wjc/dunst.logs&)
 (unclutter -idle 30&)
