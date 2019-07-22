383a35e0c0284e4f064f77b912b54d60a3622636
William <william@undefined.re>
2019-07-22 14:47:47 +0200

    change xinit vs xsession

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,33 +1,34 @@
 mkdir -p ~/.config
-ln -s ~/conf/vim/vimrc ~/.vimrc
-ln -s ~/conf/vim/gvimrc ~/.gvimrc
-ln -s ~/conf/vim ~/.vim
-ln -s ~/conf/vim ~/.config/nvim
-ln -s ~/conf/vim/vimrc ~/.config/nvim/init.vim
+ln -s ~/conf/vim/vimrc             ~/.vimrc
+ln -s ~/conf/vim/gvimrc            ~/.gvimrc
+ln -s ~/conf/vim                   ~/.vim
+ln -s ~/conf/vim                   ~/.config/nvim
+ln -s ~/conf/vim/vimrc             ~/.config/nvim/init.vim
 
-ln -s ~/conf/zsh/zshrc ~/.zshrc
-ln -s ~/conf/git/gitconfig ~/.gitconfig
-ln -s ~/conf/misc/mostrc ~/.mostrc
-ln -s ~/conf/misc/tmux.conf ~/.tmux.conf
-ln -s ~/conf/misc/vimperatorrc ~/.vimperatorrc
+ln -s ~/conf/xmonad                ~/.xmonad
+ln -s ~/conf/zsh/zshrc             ~/.zshrc
+ln -s ~/conf/git/gitconfig         ~/.gitconfig
+ln -s ~/conf/misc/tmux.conf        ~/.tmux.conf
 
-ln -s ~/conf/xmonad ~/.xmonad
+ln -s ~/conf/misc/w3m              ~/.w3m
+ln -s ~/conf/misc/mostrc           ~/.mostrc
 
-ln -s ~/conf/misc/xmodmap ~/.xmodmap
-ln -s ~/conf/misc/xinitrc ~/.xinitrc
-ln -s ~/conf/misc/xinitrc ~/.xsession
+ln -s ~/conf/misc/compton.conf     ~/.compton.conf
+ln -s ~/conf/misc/vimfx            ~/.vimfx
+
+ln -s ~/conf/misc/xinitrc          ~/.xinitrc
+ln -s ~/conf/misc/xsession         ~/.xsession
 ln -s ~/conf/misc/xsession-user.sh ~/.xsession-user.sh
-ln -s ~/conf/misc/Xdefaults ~/.Xdefaults
-ln -s ~/conf/misc/Xresources ~/.Xresources
-ln -s ~/conf/misc/XCompose ~/.XCompose
-ln -s ~/conf/misc/conkyrc ~/.conkyrc
-ln -s ~/conf/misc/compton.conf ~/.compton.conf
-ln -s ~/conf/misc/xbindkeysrc ~/.bindkeysrc
-ln -s ~/conf/misc/fonts.conf ~/.fonts.conf
-ln -s ~/conf/misc/dunstrc ~/.dunstrc
-ln -s ~/conf/misc/vimfx ~/.vimfx
-ln -s ~/conf/misc/w3m ~/.w3m
-ln -s ~/conf/misc/xscreensaver ~/.xscreensaver
+
+ln -s ~/conf/misc/XCompose         ~/.XCompose
+ln -s ~/conf/misc/Xdefaults        ~/.Xdefaults
+ln -s ~/conf/misc/Xresources       ~/.Xresources
+ln -s ~/conf/misc/xscreensaver     ~/.xscreensaver
+
+# FIXME should refresh this:
+ln -s ~/conf/misc/fonts.conf       ~/.fonts.conf
+
+#ln -s ~/conf/misc/xbindkeysrc      ~/.bindkeysrc
 
 cd ~/conf/notify-user
 make
