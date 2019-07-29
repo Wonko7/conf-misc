293ea85d5540277c0efe2c7051c63927fe695314
William <william@undefined.re>
2019-07-29 17:15:35 +0200

    prettier sm push

--- scripts/sm_g-push.zsh
+++ scripts/sm_g-push.zsh
@@ -5,8 +5,10 @@ for i in ergo git kernel-config misc vim zsh zsh/tmux-sessions zsh/bookmarks .; 
   cd $i
   b=$(git b | sed -nre "s/^\* //p")
   echo $i $b
-  (git push || echo "/!\\" $i $b failed &) >&2 &
+  #(/usr/bin/time -f "$i %e seconds" git push -q  2> $err || echo "/!\\" $i $b failed ) &
+  (/usr/bin/time -f "$i %e seconds" git push -q || echo "/!\\" $i $b failed ) &
+  if [ "$i" = "." ]; then
+    (/usr/bin/time -f ". %e seconds" git push -q --tags  || echo "/!\\" $i $b failed ) &
+  fi
   cd - > /dev/null
 done | column -t
-
-git push --tags&
