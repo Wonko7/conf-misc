4485aee3d5dfb6915aee1e3b5f1eb4dbd941b505
William <william@undefined.re>
2019-07-29 16:18:58 +0200

    add push conf in sm_g-push

--- scripts/sm_g-push.zsh
+++ scripts/sm_g-push.zsh
@@ -1,10 +1,12 @@
 #! /bin/zsh
 
 cd $conf
-for i in pass-audit ergo git kernel-config misc vim zsh zsh/tmux-sessions zsh/bookmarks; do
+for i in conf pass-audit ergo git kernel-config misc vim zsh zsh/tmux-sessions zsh/bookmarks; do
   cd $i
   b=$(git b | sed -nre "s/^\* //p")
   echo $i $b
   (git push || echo "/!\\" $i $b failed &) >&2 &
   cd - > /dev/null
 done | column -t
+
+git push --tags&
