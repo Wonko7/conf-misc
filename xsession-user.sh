f414f86b865452218addcdcc4a82b8a7d97c1097
William <william@undefined.re>
2019-09-03 18:31:05 +0200

    more sleep time for conky start up

--- xsession-user.sh
+++ xsession-user.sh
@@ -4,13 +4,21 @@ HOST=$(hostname)
 
 echo "export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" > /tmp/dbus-exports.sh
 chmod a+r /tmp/dbus-exports.sh
+local sleepy_time=3
 
 # one shot:
 #feh --bg-scale ~/pics/spideyverse-vlcsnap-2019-03-23-17h34m19s903.png
 case $HOST in
-  yggdrasill)  feh --bg-scale ~/pics/dualspidey/t3.png&;;
-  daban-urnud) feh --bg-scale /home/wjc/pics/spideyverse-vlcsnap-2019-03-23-17h34m19s903.png&;;
-  *)           logger -s -p user.error "xsession-user: unknown $HOST no background!";;
+  yggdrasill)
+    feh --bg-scale ~/pics/dualspidey/t3.png&
+    sleepy_time=3
+    ;;
+  daban-urnud)
+    feh --bg-scale /home/wjc/pics/spideyverse-vlcsnap-2019-03-23-17h34m19s903.png&
+    sleepy_time=5
+    ;;
+  *)
+    logger -s -p user.error "xsession-user: unknown $HOST no background!";;
 esac
 
 ~/conf/misc/scripts/kbd.sh&
@@ -21,12 +29,14 @@ mute&
 (conky -c ~/conf/misc/generated/$HOST.conkyrc &)&
 # ... let me explain:
 # conky needs to be restarted otherwise desktop names are all null. Then, if screen is locked before conky is restarted, conky crashes.
-(sleep 1 && killall -s USR1 conky && sleep 3 && xscreensaver-command --lock)&
+(sleep 1 && killall -s USR1 conky && sleep $sleepy_time && xscreensaver-command --lock)&
 (compton --config ~/.compton.conf&)&
 (python3.7 ~/conf/misc/systemd-lock-handler.py xscreensaver-command --lock&)&
 (dunst -config ~/conf/misc/generated/$HOST.dunstrc 2>&1 > /home/wjc/dunst.logs&)&
 (unclutter -idle 30&)&
 (qlipper&)&
+
+# sigh:
 (sleep 5 && ~/conf/misc/scripts/kbd.sh LOL1)&
 (sleep 10 && ~/conf/misc/scripts/kbd.sh LOL2)&
 (sleep 15 && ~/conf/misc/scripts/kbd.sh LOL3)&
