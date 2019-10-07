13dca9ac9dafa3bdaba1c328c1a7cc8bea553137
William <william@undefined.re>
2019-10-07 11:03:38 +0200

    s/fzf/sk/ in tmux_switch_window

--- scripts/tmux_fzf_switch_windows.sh
+++ scripts/tmux_fzf_switch_windows.sh
@@ -3,6 +3,6 @@
 if [ ! -z "$1" ]; then
   __tmux_session=$1
 fi
-win=$(tmux list-windows -F '#I:#W' | ~/.fzf/bin/fzf-tmux | cut -d: -f1)
+win=$(tmux list-windows -F '#I:#W' | sk-tmux | cut -d: -f1)
 
 tmux select-window -t "$__tmux_session:$win" 2>&1 > /dev/null
