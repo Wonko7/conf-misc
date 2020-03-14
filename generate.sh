c1d5012594e464226b34168721c1c479f343447f
William <william@undefined.re>
2020-03-14 11:30:30 +0100

    initial rocinante config

--- generate.sh
+++ generate.sh
@@ -2,12 +2,17 @@
 
 cd $(dirname $0)
 
+# so this won't scale
 # yggdrasill:
 y_border=40
 y_conky_width=250
 # daban-urnud:
 d_border=20
 d_conky_width=150
+# rocinante:
+r_border=20
+r_conky_width=150
+
 
 for i in $@; do
   case $i in
@@ -56,6 +61,22 @@ if [ ! -z $generate_conky ]; then
     -re s/@VMINILINE/10/g \
     -re s/@WLAN/wlp4s0/g \
     > generated/daban-urnud.conkyrc
+
+  sed conky/conky.conf \
+    -re s/@WIDTH/$r_conky_width/g \
+    -re s/@GAP_X/$r_border/g \
+    -re s/@TOP_WIDTH/14/g \
+    -re s/@FONT_SIZE/10/g \
+    -re s/@FONT_DATE_SIZE/9/g \
+    -re s/@FONT_DESKTOP_SIZE/10/g \
+    -re s/@V_CLOCK/122/g \
+    -re s/@VDELTA/68/g \
+    -re s/@VDDELTA/48/g \
+    -re s/@VDDDELTA/58/g \
+    -re s/@H_TOP/120/g \
+    -re s/@VMINILINE/10/g \
+    -re s/@WLAN/wlp4s0/g \
+    > generated/rocinante.conkyrc
 fi
 
 make_dunst ()
@@ -86,7 +107,8 @@ make_dunst ()
 if [ ! -z $generate_dunst ]; then
   echo dunst!
   make_dunst daban-urnud $d_conky_width $d_border 13
-  make_dunst yggdrasill $y_conky_width $y_border 20
+  make_dunst rocinante   $r_conky_width $r_border 13
+  make_dunst yggdrasill  $y_conky_width $y_border 20
 fi
 
 if [ ! -z $generate_git ]; then
