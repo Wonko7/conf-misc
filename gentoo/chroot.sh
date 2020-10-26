e3f8d6d36531c3f0285a437c9cf58ff8b2751a6a
William <william@undefined.re>
2020-10-26 13:44:40 +0100

    chroot/next

--- gentoo/chroot.sh
+++ gentoo/chroot.sh
@@ -4,11 +4,7 @@ next=/mnt/next
 stamp=$(date +%F)
 
 function die {
-    if [ ! -z "$1" ] ; then
-        exit $1
-    else
-        exit 1
-    fi
+    exit $1
 }
 
 function init {
@@ -17,24 +13,25 @@ function init {
     if [ ! -d /mnt/gentoo/_live/next ]; then
         echo create?
         read ans
-        [ "$ans" != y && "$ans" != yes ] && die 5
-        btrfs subvolume snapshot /mnt/gentoo/_live/root /mnt/gentoo/_live/next
-        mount /dev/nvme0n1p4 /mnt/next -osubvol=_live/next
+        [ "$ans" != y -a "$ans" != yes ] && die 5
+        btrfs subvolume snapshot /mnt/gentoo/_live/root /mnt/gentoo/_live/next || die 5
+        #sleep 0.5
+        mount /dev/nvme0n1p4 /mnt/next -osubvol=_live/next || die 6
         sed -i /mnt/next/etc/os-release -re "s/^RELEASE_DATE=.*/RELEASE_DATE=\"init: $stamp\"/"
     else
         mount /dev/nvme0n1p4 /mnt/next -osubvol=_live/next
     fi
 
-    mount --types proc /proc /mnt/gentoo/proc
-    mount --rbind /sys /mnt/gentoo/sys
-    mount --make-rslave /mnt/gentoo/sys
-    mount --rbind /dev /mnt/gentoo/dev
-    mount --make-rslave /mnt/gentoo/dev
-    mount --rbind /run /mnt/gentoo/run
+    mount --types proc /proc /mnt/next/proc
+    mount --rbind /sys /mnt/next/sys
+    mount --make-rslave /mnt/next/sys
+    mount --rbind /dev /mnt/next/dev
+    mount --make-rslave /mnt/next/dev
+    mount --rbind /run /mnt/next/run
 
-    mount /dev/nvme0n1p4 /mnt/gentoo/home -osubvol=_live/home
+    mount /dev/nvme0n1p4 /mnt/next/home -osubvol=_live/home
 
-    env -u HOME chroot --userspec wjc:wjc /mnt/gentoo /bin/zsh
+    env -u HOME chroot --userspec wjc:wjc /mnt/next /bin/zsh
 }
 
 function finalize {
