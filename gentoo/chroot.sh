0f2d7cfd5c01c818dd7f650d112073360d55d637
William <william@undefined.re>
2021-01-02 20:36:43 +0100

    update scripts to new btrfs naming convention

--- gentoo/chroot.sh
+++ gentoo/chroot.sh
@@ -2,16 +2,16 @@
 
 HOST=`hostname`
 case $HOST in
-    yggdrasill)
-        root_name=gentoo-root
-        home_name=gentoo-home
-        device=/dev/nvme0n1p4
-    ;;
+    yggdrasill|nostromo)
+        root_name=@root
+        home_name=@home
+        device=/dev/mapper/vault
+        ;;
     rocinante)
         root_name=root
         home_name=home
         device=/dev/nvme0n1p4
-    ;;
+        ;;
     *)
         echo missing config for $HOST
         exit 1
@@ -27,15 +27,15 @@ function die {
 function init {
     echo mounting /mnt/next
 
-    if [ ! -d /mnt/gentoo/_live/next ]; then
+    if [ ! -d /mnt/gentoo/_live/@next ]; then
         echo create?
         read ans
         [ "$ans" != y -a "$ans" != yes ] && die 5
-        btrfs subvolume snapshot /mnt/gentoo/_live/$root_name /mnt/gentoo/_live/next || die 5
-        mount $device /mnt/next -osubvol=_live/next || die 6
+        btrfs subvolume snapshot /mnt/gentoo/_live/$root_name /mnt/gentoo/_live/@next || die 5
+        mount $device /mnt/next -osubvol=_live/@next || die 6
         sed -i /mnt/next/etc/os-release -re "s/^RELEASE_DATE=.*/RELEASE_DATE=\"init: $stamp\"/" # FIXME create too.
     else
-        mount $device /mnt/next -osubvol=_live/next || echo "something is already mounted on next, pretending that's ok"
+        mount $device /mnt/next -osubvol=_live/@next || echo "something is already mounted on next, pretending that's ok"
     fi
 
     mount --types proc /proc /mnt/next/proc
@@ -51,14 +51,14 @@ function init {
 }
 
 function finalize {
-    if [ ! -d /mnt/gentoo/_live/next ]; then
+    if [ ! -d /mnt/gentoo/_live/@next ]; then
         die 33
     fi
 
     sed -i /mnt/next/etc/os-release -re "s/^(RELEASE_DATE=.*)\"/\1 release: $stamp\"/"
     
     mv /mnt/gentoo/_live/$root_name /mnt/gentoo/_old/${root_name}_${stamp}
-    mv /mnt/gentoo/_live/next /mnt/gentoo/_live/$root_name
+    mv /mnt/gentoo/_live/@next /mnt/gentoo/_live/$root_name
 }
 
 $@
