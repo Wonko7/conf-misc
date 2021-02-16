06dbdd62cc5e85e97c96ebb6b27976537b5ce110
William <william@undefined.re>
2021-02-16 22:38:29 +0100

    net script handles vpn endpoints in a variable

--- gentoo/net.sh
+++ gentoo/net.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+vpn_endpoint=azirevpn-nl1
+
 start ()
 {
   ip netns add out
@@ -9,7 +11,7 @@ start ()
   ip -n rawdog link set wgout0 netns out
   #ip netns exec out wg setconf wgout0 /etc/wireguard/azirevpn-se1.conf
   #ip netns exec out wg setconf wgout0 /etc/wireguard/azirevpn-no1.conf
-  ip netns exec out wg setconf wgout0 /etc/wireguard/azirevpn-nl1.conf
+  ip netns exec out wg setconf wgout0 /etc/wireguard/${vpn_endpoint}.conf
 
   ip -n rawdog link add wg42 type wireguard
   ip -n rawdog link set wg42 netns 1
@@ -29,7 +31,7 @@ start ()
 
   # up up and away: rawdog
   ip netns exec rawdog dhcpcd -b wlan0
-  #ip netns exec rawdog dhcpcd -b eth0
+  ip netns exec rawdog dhcpcd -b eth0
   ip netns exec rawdog dhcpcd -b eth1
   ip netns exec rawdog wpa_supplicant -B -c/etc/wpa_supplicant/wpa_supplicant-wlan0.conf -iwlan0 # FIXME
 
@@ -38,9 +40,9 @@ start ()
   ip addr add 10.42.0.6/24 dev wg42
   ip ro add 10.42.0.0/24 via 10.42.0.1 dev wg42
 
-  # up up and away: wg out
+  # up up and away: wgout0
   ip -n out link set wgout0 up
-  ip -n out addr add 10.0.23.23/19 dev wgout0
+  ip netns exec out sh /etc/wireguard/${vpn_endpoint}.ip.sh
   ip -n out ro add default dev wgout0
 
   # setup localhost everywhere:
@@ -69,6 +71,19 @@ restart ()
 {
   stop
   start
+}
+
+init_azire ()
+{
+  cd /etc/wireguard
+  for c in azirevpn-*; do
+    ipconf=$(echo $c | sed -re 's/\.conf/.ip.sh/')
+    sed -i $c -re 's/^(Address|DNS)/#\1/'
+    ip4=$(sed /etc/wireguard/azirevpn-dk1.conf -nre 's;#Address = ([^,]+), (.+)$;\1;p')
+    ip6=$(sed /etc/wireguard/azirevpn-dk1.conf -nre 's;#Address = ([^,]+), (.+)$;\2;p')
+    echo "ip addr add $ip4 dev wgout0" > $ipconf
+    echo "ip addr add $ip6 dev wgout0" >> $ipconf
+  done
 }
 
 $@
