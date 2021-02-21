f26e9d95b99978b8c1ea5b036ab275101ab8ca4e
William <william@undefined.re>
2021-02-21 14:40:26 +0100

    generalise net.sh: discover wlan & phy device names

--- gentoo/net.sh
+++ gentoo/net.sh
@@ -2,11 +2,13 @@
 
 vpn_endpoint=azirevpn-nl1
 lwg_interfaces="wg42 wg69"
-eth_interfaces=$(ip -o li | cut -d: -f2 | grep 'eth')
 #phy_interfaces=$(ip -o li | cut -d: -f2 | egrep '(wlan|eth)')
 
 start ()
 {
+  eth_interfaces=$(ip -o li | cut -d: -f2 | grep 'eth')
+  wlan_interfaces=$(ip -o li | cut -d: -f2 | grep 'wlan')
+  wphy_interfaces=$(iw phy | sed -nre 's/^Wiphy //p')
   ip netns add out
   ip netns add rawdog
 
@@ -15,6 +17,7 @@ start ()
   ip netns exec out wg setconf wgout0 /etc/wireguard/${vpn_endpoint}.conf
 
   for wg in $lwg_interfaces; do
+    echo @@ starting $wg
     ip -n rawdog link add $wg type wireguard
     ip -n rawdog link set $wg netns 1
     wg setconf $wg /etc/wireguard/${wg}.conf
@@ -22,24 +25,36 @@ start ()
 
   # isolate physical interfaces:
   for eth in $eth_interfaces; do
+    echo @@ starting $eth
     ip link set $eth down
     ip link set $eth netns rawdog
   done
-  ip link set wlan0 down
-  iw phy phy0 set netns name rawdog
+  for wlan in $wlan_interfaces; do
+    echo @@ starting $wlan
+    ip link set $wlan down
+  done
+  for phy in $wphy_interfaces; do
+    echo @@ starting $phy
+    iw phy $phy set netns name rawdog
+  done
+
+  ip netns exec rawdog /usr/libexec/iwd&
+  wlan_interfaces=$(ip -n rawdog -o li | cut -d: -f2 | grep 'wlan')
+  # FIXME: wlan gets renamed at this point for some reason
+  sleep 1
+  eth_interfaces=$(ip -n rawdog -o li | cut -d: -f2 | grep 'eth')
+  wlan_interfaces=$(ip -n rawdog -o li | cut -d: -f2 | grep 'wlan')
 
   # up up and away: rawdog
-  ip netns exec rawdog /usr/libexec/iwd&
-  ip netns exec rawdog dhcpcd -b wlan0
-  for eth in $eth_interfaces; do
-    ip netns exec rawdog dhcpcd -b $eth
-    echo yes
+  for i in $eth_interfaces $wlan_interfaces; do
+    echo @@ starting dhcp on $i
+    ip netns exec rawdog dhcpcd -b $i
   done
   #ip netns exec rawdog wpa_supplicant -B -c/etc/wpa_supplicant/wpa_supplicant-wlan0.conf -iwlan0
 
   # up up and away: wg42 & wg69
   for wg in $lwg_interfaces; do
-    #ip a
+    echo @@ starting ip conf on $wg
     ip link set $wg up
     sh /etc/wireguard/${wg}.ip.sh
   done
@@ -58,19 +73,37 @@ start ()
 
 stop ()
 {
+  eth_interfaces=$(ip -n rawdog -o li | cut -d: -f2 | grep 'eth')
+  wlan_interfaces=$(ip -n rawdog -o li | cut -d: -f2 | grep 'wlan')
+  wphy_interfaces=$(ip netns exec rawdog iw phy | sed -nre 's/^Wiphy //p')
+
   killall dhcpcd || true
   killall dhcpcd iwd wpa_supplicant || true
   for eth in $eth_interfaces; do
+    echo @@ stopping $eth
     ip -n rawdog link set $eth down
     ip -n rawdog link set $eth netns 1
   done
-  ip -n rawdog link set wlan0 down
-  ip netns exec rawdog iw phy phy0 set netns 1
+
+  for wlan in $wlan_interfaces; do
+    echo @@ stopping $wlan
+    ip -n rawdog link set $wlan down
+  done
+  for phy in $wphy_interfaces; do
+    echo @@ stopping $phy
+    ip netns exec rawdog iw phy $phy set netns 1
+  done
 
   for wg in $lwg_interfaces; do
     ip li delete $wg
   done
   ip -n out li delete wgout0
+
+  if [ "`hostname`" = "enterprise" ]; then
+    echo @@ enterprise wifi driver reload
+    rmmod iwlmvm iwlwifi
+    modprobe iwlwifi
+  fi
 }
 
 restart ()
