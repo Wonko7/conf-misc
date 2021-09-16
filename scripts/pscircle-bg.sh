9397f3959fd36da5da77ead3dc158764ef5146df
William <william@undefined.re>
2021-09-16 13:26:34 +0200

    tint2 & pscircle

--- scripts/pscircle-bg.sh
+++ scripts/pscircle-bg.sh
@@ -4,8 +4,8 @@
 
 #bg_in=/data/docs/pics/wallpapers/spacex2/Space-X-falcon-heavy-space-rocket-Quad-HD-wallpapers-2.jpg&
 bg_in="/data/docs/pics/wallpapers/nasa-poster-vision-future/1 - 8XMgqaI.png"
-dir=/tmp/.${USER}-wallpaper
 bg_out=/tmp/.${USER}-wallpaper/w.png
+dir=/tmp/.${USER}-wallpaper
 mkdir -p $dir
 #!/usr/bin/env bash
 
@@ -40,17 +40,19 @@ while [ 1 ]; do
       --toplists-bar-background=444444 \
       --toplists-bar-color=87d2ff \
       --max-children=55 \
-      --tree-radius-increment=110 \
-      --dot-radius=3 \
+      --tree-radius-increment=250 \
+      --dot-radius=9 \
       --link-width=1.3 \
-      --tree-font-face="Clear Sans Medium" \
+      --tree-font-face="JetBrainsMono Nerd Font Mono" \
+      --toplists-font-face="JetBrainsMono Nerd Font Mono" \
       --tree-font-size=33 \
-      --toplists-font-size=33 \
-      --toplists-bar-width=30 \
-      --toplists-row-height=15 \
-      --toplists-bar-height=3 \
-      --cpulist-center=400.0:-80.0 \
-      --memlist-center=400.0:80.0 \
+      --toplists-font-size=35 \
+      --toplists-bar-width=90 \
+      --toplists-row-height=45 \
+      --toplists-bar-height=9 \
+      --toplists-column-padding=45 \
+      --cpulist-center=1300.0:-300.0 \
+      --memlist-center=1300.0:200.0 \
       --interval=0 \
       --output=$bg_out
 
